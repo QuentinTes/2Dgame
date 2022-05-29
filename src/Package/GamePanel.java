999b4fdc8d45db20d46d6f2445bc00096df1505d
Quentin <quentin.testaferrata@gmail.com>
2022-05-29 13:46:40 +0200

    Feature: GameThread

--- src/Package/GamePanel.java
+++ src/Package/GamePanel.java
@@ -3,7 +3,7 @@ package Package;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
     //Screen settings
     final int originalTileSize = 16; //16x16 tiles
     final int scale = 3;
@@ -14,9 +14,21 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; //768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    Thread gameThread; //keeps program running
+
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this); //this class
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        //Game Loop
     }
 }
